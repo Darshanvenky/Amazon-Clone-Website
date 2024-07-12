28d9f0790fb7ec8ca27495369294fea8fdb03672
DarshanVenky <darshanvenky07@gmail.com>
2024-07-12 19:34:49 +0530

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -10,6 +10,8 @@ import {renderPaymentSummary} from './checkout/paymentsSummary.js';
 
 //import '../data/car.js';
 
+import '../data/backend-practice.js';
+
 
 renderCheckoutHeader();
 renderOrderSummary();
